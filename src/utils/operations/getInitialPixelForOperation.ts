9df22dcd59c845c5322d9bc3a9d4d6158ebdf84b
Ríad Oliveira <riad.oliveira@hotmail.com>
2023-09-26 17:25:34 -0300

    getInitialPixelForOperation improvement

--- src/utils/operations/getInitialPixelForOperation.ts
+++ src/utils/operations/getInitialPixelForOperation.ts
@@ -7,7 +7,7 @@ export const getInitialPixelForOperation = ({
     const lowerDimension = Math.max(width, height);
     const higherDimension = Math.max(width, height);
 
-    const ratio = lowerDimension / higherDimension;
+    const ratio = lowerDimension / (higherDimension * Math.E);
     const pixelIndex = Math.round(ratio * pixels.length);
 
     return pixelIndex - (pixelIndex % 4);
