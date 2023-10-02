270207e91c815403619ce37385bcfb2120bd51fa
Ríad Oliveira <riad.oliveira@hotmail.com>
2023-10-02 15:54:34 -0300

    getNextPixelForOperation fixed

--- src/utils/operations/getNextPixelForOperation.ts
+++ src/utils/operations/getNextPixelForOperation.ts
@@ -17,18 +17,22 @@ export const getNextPixelForOperation = ({
     operationInd,
     imageDataLength,
 }: GetNextPixelForOperationProps) => {
-    if (currentPixelInd >= initialPixelInd) {
-        const pixelJump = PIXEL_JUMPS[operationInd % PIXEL_JUMPS.length];
-        const updatedInd = currentPixelInd + pixelJump;
-
-        if (updatedInd < imageDataLength) return updatedInd;
-        return Number(!(currentPixelInd % 2));
-    }
-
+    const currentLowerThanOriginal = currentPixelInd < initialPixelInd;
     const difference = initialPixelInd - currentPixelInd;
-    if (difference === COMPARABLE_DIFFERENCE) {
+
+    if (currentLowerThanOriginal && difference === COMPARABLE_DIFFERENCE) {
         throw new AppError('Unable to get next pixel, length limit reached.');
     }
 
-    return currentPixelInd + PIXEL_JUMPS[0];
+    const currentParity = currentPixelInd % 2;
+    const initialParity = initialPixelInd % 2;
+
+    const hasDifferentParity = currentParity != initialParity;
+    const jumpInd =
+        currentLowerThanOriginal || hasDifferentParity
+            ? 0
+            : operationInd % PIXEL_JUMPS.length;
+
+    const updatedInd = currentPixelInd + PIXEL_JUMPS[jumpInd];
+    return updatedInd < imageDataLength ? updatedInd : Number(!currentParity);
 };
